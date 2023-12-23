2c852d4003a5c56233bb47b21fe5ac59a28e1980
amirisback <faisalamircs@gmail.com>
2023-12-23 14:13:32 +0700

    update: prepare intro

---
+++ app/src/main/java/io/github/amirisback/retrosnake/MainActivity.java
@@ -0,0 +1,39 @@
+package io.github.amirisback.retrosnake;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+/**
+ * Created by faisalamircs on 23/12/2023
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, initializationStatus -> {
+
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+    }
+}
